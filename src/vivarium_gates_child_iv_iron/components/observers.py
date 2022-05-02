ccf434195c74340faaf6fa60b0eac8d93d641a65
albrja <albrja@uw.edu>
2022-05-02 16:29:19 -0700

    Updating observer and post processing to get rid of age stratification

--- src/vivarium_gates_child_iv_iron/components/observers.py
+++ src/vivarium_gates_child_iv_iron/components/observers.py
@@ -64,7 +64,7 @@ class BirthObserver:
     configuration_defaults = {
         "observers": {
             "birth": {
-                "exclude": [],
+                "exclude": ["age"],
                 "include": [],
             }
         }

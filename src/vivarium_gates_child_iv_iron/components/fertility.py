672a362efad9165f777c525b7e9efa70687c3150
albrja <albrja@uw.edu>
2022-05-18 15:44:20 -0700

    Adding functionality in fertility component to load existing birth records

--- src/vivarium_gates_child_iv_iron/components/fertility.py
+++ src/vivarium_gates_child_iv_iron/components/fertility.py
@@ -7,7 +7,9 @@ Fertility module to create simulants from existing data
 
 """
 import numpy as np
+import glob
 import pandas as pd
+from pathlib import Path
 
 from vivarium_public_health import utilities
 from vivarium_public_health.population.data_transformations import (
@@ -25,10 +27,11 @@ class FertilityFromOutputs:
 
     configuration_defaults = {
         "fertility": {
-            # todo: would this be a good place to flag whether we are using dummy data or real output data?
+            "fertility_input_data_path": ""
         }
     }
 
+    columns_created = ["age", "sex", "alive", "entrance_time", "exit_time"] # not sure if we need location?
     def __repr__(self):
         return "FertilityFromOutputs()"
 
@@ -38,10 +41,24 @@ class FertilityFromOutputs:
 
     def setup(self, builder):
         self.clock = builder.time.clock()
+        self.fertility_data_directory = builder.fertility.fertility_input_data_path
         self.randomness = builder.randomness
         self.simulant_creator = builder.population.get_simulant_creator()
 
+        builder.population.initializes_simulants(
+            self.on_initialize_simulants, creates_columns=self.columns_created
+        )
         builder.event.register_listener("time_step", self.on_time_step)
+
+    def on_initialize_simulants(self) -> None:
+        """
+        Method to load existing fertility data to use as birth records.
+        """
+#        fertility_data_dir = self.fertility_data_directory
+        fertility_data_dir = "/home/albrja/scratch/data/iv_iron_child/"
+        file_path = glob.glob(fertility_data_dir + 'south_asia*.hdf')[0]
+        # todo: fix path names to incorporate what draw and seed to use
+        self.birth_records = pd.read_hdf(file_path)
 
     def on_time_step(self, event):
         """Adds new simulants every time step determined by a simulant's birth date in the output data.
@@ -50,10 +67,13 @@ class FertilityFromOutputs:
         event
             The event that triggered the function call.
         """
-        
-        simulants_to_add = ()
+        birth_records = self.birth_records
+        born_previous_step_idx = (birth_records['birth_date'] < self.clock()) & (
+            birth_records['birth_date'] > self.clock() - event.step_size)
+        simulants_to_add = birth_records[born_previous_step_idx]
 
-        if simulants_to_add > 0:
+        # todo: update pop config with new columns
+        if len(simulants_to_add) > 0:
             self.simulant_creator(
                 simulants_to_add,
                 population_configuration={

69f9d8b4bbbe54e531275213a86ce19bb66692b8
albrja <albrja@uw.edu>
2022-05-18 16:47:16 -0700

    Changing method to read in data to not be in on initialize simulants

--- src/vivarium_gates_child_iv_iron/components/fertility.py
+++ src/vivarium_gates_child_iv_iron/components/fertility.py
@@ -31,7 +31,6 @@ class FertilityFromOutputs:
         }
     }
 
-    columns_created = ["age", "sex", "alive", "entrance_time", "exit_time"] # not sure if we need location?
     def __repr__(self):
         return "FertilityFromOutputs()"
 
@@ -41,24 +40,23 @@ class FertilityFromOutputs:
 
     def setup(self, builder):
         self.clock = builder.time.clock()
+        self.birth_records = self._get_birth_records()
         self.fertility_data_directory = builder.fertility.fertility_input_data_path
         self.randomness = builder.randomness
         self.simulant_creator = builder.population.get_simulant_creator()
 
-        builder.population.initializes_simulants(
-            self.on_initialize_simulants, creates_columns=self.columns_created
-        )
         builder.event.register_listener("time_step", self.on_time_step)
 
-    def on_initialize_simulants(self) -> None:
+    def _get_birth_records(self) -> pd.DataFrame:
         """
         Method to load existing fertility data to use as birth records.
         """
-#        fertility_data_dir = self.fertility_data_directory
-        fertility_data_dir = "/home/albrja/scratch/data/iv_iron_child/"
+        fertility_data_dir = self.fertility_data_directory
         file_path = glob.glob(fertility_data_dir + 'south_asia*.hdf')[0]
         # todo: fix path names to incorporate what draw and seed to use
-        self.birth_records = pd.read_hdf(file_path)
+        birth_records = pd.read_hdf(file_path)
+
+        return birth_records
 
     def on_time_step(self, event):
         """Adds new simulants every time step determined by a simulant's birth date in the output data.
@@ -68,18 +66,19 @@ class FertilityFromOutputs:
             The event that triggered the function call.
         """
         birth_records = self.birth_records
-        born_previous_step_idx = (birth_records['birth_date'] < self.clock()) & (
+        born_previous_step_mask = (birth_records['birth_date'] < self.clock()) & (
             birth_records['birth_date'] > self.clock() - event.step_size)
-        simulants_to_add = birth_records[born_previous_step_idx]
+        born_previous_step = birth_records[born_previous_step_mask]
+        born_previous_step_idx = born_previous_step.index
+        simulants_to_add = len(born_previous_step)
 
-        # todo: update pop config with new columns
-        if len(simulants_to_add) > 0:
+        if simulants_to_add > 0:
             self.simulant_creator(
                 simulants_to_add,
                 population_configuration={
                     "age_start": 0,
                     "age_end": 0,
                     "sim_state": "time_step",
+                    "new_births": born_previous_step_idx
                 },
             )
-

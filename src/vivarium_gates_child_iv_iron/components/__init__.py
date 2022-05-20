595707eac984e6d82131595b677dfb90cba22086
albrja <albrja@uw.edu>
2022-05-20 12:57:57 -0700

    Updating __init__ to include new components

--- src/vivarium_gates_child_iv_iron/components/__init__.py
+++ src/vivarium_gates_child_iv_iron/components/__init__.py
@@ -1,1 +1,4 @@
+from vivarium_gates_child_iv_iron.components.fertility import FertilityLineList
+from vivarium_gates_child_iv_iron.components.lbwsg import LBWSGLineList
 from vivarium_gates_child_iv_iron.components.observers import BirthObserver, ResultsStratifier
+from vivarium_gates_child_iv_iron.components.population import PopulationLineList

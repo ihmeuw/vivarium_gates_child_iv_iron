f6b0694a197e30bb7b1abbbf3b1f5b0b80417f3d
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2022-06-03 15:14:49 -0700

    store intervention coverage input data in shared location

--- src/vivarium_gates_child_iv_iron/constants/paths.py
+++ src/vivarium_gates_child_iv_iron/constants/paths.py
@@ -8,6 +8,6 @@ BASE_DIR = Path(vivarium_gates_child_iv_iron.__file__).resolve().parent
 ARTIFACT_ROOT = Path(f"/share/costeffectiveness/artifacts/{metadata.PROJECT_NAME}/")
 MODEL_SPEC_DIR = BASE_DIR / 'model_specifications'
 RESULTS_ROOT = Path(f'/share/costeffectiveness/results/{metadata.PROJECT_NAME}/')
-MATERNAL_INTERVENTION_COVERAGE_CSV = '/ihme/homes/albrja/repos/vivarium_research_iv_iron/misc_investigations/simulation_intervention_coverage.csv'
+MATERNAL_INTERVENTION_COVERAGE_CSV = '/ihme/costeffectiveness/artifacts/vivarium_gates_child_iv_iron/input_data/simulation_intervention_coverage.csv'
 
 TEMPORARY_PAF_DIR = Path('/share/costeffectiveness/auxiliary_data/GBD_2019/01_original_data/population_attributable_fraction/risk_factor/lbwsg/data/')

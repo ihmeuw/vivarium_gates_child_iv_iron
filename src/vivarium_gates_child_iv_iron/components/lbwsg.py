d54b33934996a6a00ec7e78ee4231febcf3a1af9
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2022-06-03 12:44:58 -0700

    update lbwsg implementation to utilize birth exposure pipeline

--- src/vivarium_gates_child_iv_iron/components/lbwsg.py
+++ src/vivarium_gates_child_iv_iron/components/lbwsg.py
@@ -7,11 +7,15 @@ This is a module to subclass the LBWSG component in Vivrium Public Health to use
 simulants who are initialized from line list data.
 
 """
+from typing import Dict
+
 import pandas as pd
 
 from vivarium.framework.engine import Builder
 from vivarium.framework.population import SimulantData
 from vivarium.framework.time import get_time_stamp
+from vivarium.framework.values import Pipeline
+from vivarium_public_health.risks.data_transformations import get_exposure_post_processor
 from vivarium_public_health.risks.implementations.low_birth_weight_and_short_gestation import LBWSGRisk
 
 
@@ -20,7 +24,7 @@ class LBWSGLineList(LBWSGRisk):
     Component to initialize low birthweight and short gestation data for simulants based on existing line list data.
     """
     @property
-    def name(self)-> str:
+    def name(self) -> str:
         return "line_list_low_birth_weight_and_short_gestation"
 
     def __repr__(self):
@@ -31,17 +35,38 @@ class LBWSGLineList(LBWSGRisk):
         super().setup(builder)
         self.start_time = get_time_stamp(builder.configuration.time.start)
 
+    def _get_birth_exposure_pipelines(self, builder: Builder) -> Dict[str, Pipeline]:
+        def get_pipeline(axis_: str):
+            return builder.value.register_value_producer(
+                self.birth_exposure_pipeline_name(axis_),
+                source=lambda index: self._get_birth_exposure(axis_, index),
+                preferred_post_processor=get_exposure_post_processor(builder, self.risk),
+            )
+
+        return {
+            self.birth_exposure_pipeline_name(axis): get_pipeline(axis) for axis in self.AXES
+        }
+
+    ########################
+    # Event-driven methods #
+    ########################
+
+    # noinspection PyAttributeOutsideInit
     def on_initialize_simulants(self, pop_data: SimulantData) -> None:
 
-        columns = ["birth_weight_exposure", "gestational_age_exposure"]
-        new_simulants = pd.DataFrame(columns=columns, index=pop_data.index)
+        if pop_data.creation_time < self.start_time:
+            columns = [self.exposure_column_name(axis) for axis in self.AXES]
+            new_simulants = pd.DataFrame(columns=columns, index=pop_data.index)
+            self.population_view.update(new_simulants)
+        else:
+            self.new_births = pop_data.user_data["new_births"]
+            self.new_births.index = pop_data.index
 
-        if pop_data.creation_time >= self.start_time:
-            new_births = pop_data.user_data["new_births"]
-            new_births.index = pop_data.index
+            super().on_initialize_simulants(pop_data)
 
-            # Create columns for state table
-            new_simulants["birth_weight_exposure"] = new_births["birth_weight"]
-            new_simulants["gestational_age_exposure"] = new_births["gestational_age"]
+    ##################################
+    # Pipeline sources and modifiers #
+    ##################################
 
-        self.population_view.update(new_simulants)
+    def _get_birth_exposure(self, axis: str, index: pd.Index) -> pd.Series:
+        return self.new_births.loc[index, axis]

a0172c96cf9d81129fb750572a1a1a80f69205c4
Rajan Mudambi <11376379+rmudambi@users.noreply.github.com>
2022-06-17 11:52:43 -0700

    take only first two

--- src/vivarium_gates_child_iv_iron/components/fertility.py
+++ src/vivarium_gates_child_iv_iron/components/fertility.py
@@ -69,8 +69,8 @@ class FertilityLineList:
         birth_records = self.birth_records[event.time.year]
         born_previous_step_mask = (birth_records['birth_date'] < self.clock()) & (
             birth_records['birth_date'] > self.clock() - event.step_size)
-        born_previous_step = birth_records[born_previous_step_mask]
-        # born_previous_step = birth_records.head(2)
+        # born_previous_step = birth_records[born_previous_step_mask]
+        born_previous_step = birth_records.head(2)
         simulants_to_add = len(born_previous_step)
 
         if simulants_to_add > 0:

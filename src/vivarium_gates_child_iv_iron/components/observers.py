629050ce415564c1f7d4b7d1dbbf00dd567871cd
albrja <albrja@uw.edu>
2022-07-11 12:19:51 -0700

    Including maternal supplementation for cgf stratifications

--- src/vivarium_gates_child_iv_iron/components/observers.py
+++ src/vivarium_gates_child_iv_iron/components/observers.py
@@ -67,26 +67,26 @@ class ResultsStratifier(ResultsStratifier_):
             mapper=self.child_growth_risk_factor_stratification_mapper,
         )
 
-        # self.setup_stratification(
-        #     builder,
-        #     name="maternal_supplementation",
-        #     sources=[Source("maternal_supplementation_exposure", SourceType.COLUMN)],
-        #     categories={"uncovered", "ifa", "mms", "bep"},
-        # )
-        #
-        # self.setup_stratification(
-        #     builder,
-        #     name="iv_iron",
-        #     sources=[Source("iv_iron_exposure", SourceType.COLUMN)],
-        #     categories={"uncovered", "covered"},
-        # )
-        #
-        # self.setup_stratification(
-        #     builder,
-        #     name="bmi_anemia",
-        #     sources=[Source("maternal_bmi_anemia_exposure", SourceType.COLUMN)],
-        #     categories={"cat4", "cat3", "cat2", "cat1"},
-        # )
+        self.setup_stratification(
+            builder,
+            name="maternal_supplementation",
+            sources=[Source("maternal_supplementation_exposure", SourceType.COLUMN)],
+            categories={"uncovered", "ifa", "mms", "bep"},
+        )
+
+        self.setup_stratification(
+            builder,
+            name="iv_iron",
+            sources=[Source("iv_iron_exposure", SourceType.COLUMN)],
+            categories={"uncovered", "covered"},
+        )
+
+        self.setup_stratification(
+            builder,
+            name="bmi_anemia",
+            sources=[Source("maternal_bmi_anemia_exposure", SourceType.COLUMN)],
+            categories={"cat4", "cat3", "cat2", "cat1"},
+        )
 
     ###########################
     # Stratifications Details #

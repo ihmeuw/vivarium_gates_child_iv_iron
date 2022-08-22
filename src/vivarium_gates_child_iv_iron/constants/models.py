234d22cb143b359d43860fee3c4bdeda8622b6a2
albrja <albrja@uw.edu>
2022-08-22 12:51:50 -0700

    Uncommenting

--- src/vivarium_gates_child_iv_iron/constants/models.py
+++ src/vivarium_gates_child_iv_iron/constants/models.py
@@ -28,19 +28,19 @@ class __SISModel:
 # Disease Model variables #
 ###########################
 
-# DIARRHEA = __SISModel(data_keys.DIARRHEA.name)
-# LRI = __SISModel(data_keys.LRI.name)
-# MEASLES = __SISModel(data_keys.MEASLES.name)
-# MODERATE_PEM = __SISModel(data_keys.MODERATE_PEM.name)
-# SEVERE_PEM = __SISModel(data_keys.SEVERE_PEM.name)
+DIARRHEA = __SISModel(data_keys.DIARRHEA.name)
+LRI = __SISModel(data_keys.LRI.name)
+MEASLES = __SISModel(data_keys.MEASLES.name)
+MODERATE_PEM = __SISModel(data_keys.MODERATE_PEM.name)
+SEVERE_PEM = __SISModel(data_keys.SEVERE_PEM.name)
 
-# CAUSE_MODELS: List[__SISModel] = [
-#     DIARRHEA,
-#     LRI,
-#     MEASLES,
-#     MODERATE_PEM,
-#     SEVERE_PEM
-# ]
+CAUSE_MODELS: List[__SISModel] = [
+    DIARRHEA,
+    LRI,
+    MEASLES,
+    MODERATE_PEM,
+    SEVERE_PEM
+]
 
-# STATES = tuple(state for model in CAUSE_MODELS for state in model.STATES)
-# TRANSITIONS = tuple(state for model in CAUSE_MODELS for state in model.TRANSITIONS)
+STATES = tuple(state for model in CAUSE_MODELS for state in model.STATES)
+TRANSITIONS = tuple(state for model in CAUSE_MODELS for state in model.TRANSITIONS)

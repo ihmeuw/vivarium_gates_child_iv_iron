95d581ae830ac0f81040bbd109c4ffe7960ef53a
albrja <albrja@uw.edu>
2022-08-22 11:01:59 -0700

    Updating config files for final runs

--- src/vivarium_gates_child_iv_iron/constants/results.py
+++ src/vivarium_gates_child_iv_iron/constants/results.py
@@ -106,7 +106,7 @@ NON_COUNT_TEMPLATES = [
 
 POP_STATES = ('living', 'dead', 'tracked', 'untracked')
 SEXES = ('male', 'female')
-YEARS = tuple(range(2022, 2028))
+YEARS = tuple(range(2019, 2041))
 AGE_GROUPS = (
     'early_neonatal',
     'late_neonatal',

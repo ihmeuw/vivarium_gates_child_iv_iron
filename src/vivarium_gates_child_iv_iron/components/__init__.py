7f7480c633f81005f61de409c5726ac0bda6135b
Hussain Jafari <hussainsjafari@gmail.com>
2022-08-30 12:02:50 -0700

    Missing moderate PEM YLDs bugfix (#49)
    Missing moderate PEM YLDs bugfix
    Description
    
        Category: bugfix, observers
        JIRA issue: MIC-3361
    
    Explicitly include moderate PEM as cause component in disability observer even if disability weights are all 0.
    
    Verification and Testing
    Ran single run that was previously not producing YLDs and verified that it was producing desired moderate PEM YLDs.

--- src/vivarium_gates_child_iv_iron/components/__init__.py
+++ src/vivarium_gates_child_iv_iron/components/__init__.py
@@ -6,5 +6,5 @@ from vivarium_gates_child_iv_iron.components.maternal_characteristics import (
     BirthWeightShiftEffect,
     MaternalCharacteristics,
 )
-from vivarium_gates_child_iv_iron.components.observers import BirthObserver, ResultsStratifier
+from vivarium_gates_child_iv_iron.components.observers import BirthObserver, DisabilityObserver, ResultsStratifier
 from vivarium_gates_child_iv_iron.components.population import PopulationLineList

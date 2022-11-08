58c4d4647b3fdf71bee0c153238fabfa083d3177
Hussain Jafari <hjafari@uw.edu>
2022-11-08 11:45:28 -0800

    consistent quotes

--- src/vivarium_gates_child_iv_iron/constants/paths.py
+++ src/vivarium_gates_child_iv_iron/constants/paths.py
@@ -6,10 +6,10 @@ from vivarium_gates_child_iv_iron.constants import metadata
 BASE_DIR = Path(vivarium_gates_child_iv_iron.__file__).resolve().parent
 
 ARTIFACT_ROOT = BASE_DIR / "artifacts"
-MODEL_SPEC_DIR = BASE_DIR / 'model_specifications'
+MODEL_SPEC_DIR = BASE_DIR / "model_specifications"
 RAW_DATA_DIR = BASE_DIR / "data/raw"
 
 MATERNAL_INTERVENTION_COVERAGE_CSV = RAW_DATA_DIR / "simulation_intervention_coverage.csv"
 
-TEMPORARY_PAF_DIR = Path('/share/costeffectiveness/auxiliary_data/GBD_2019/01_original_data/population_attributable_fraction/risk_factor/lbwsg/data/')
+TEMPORARY_PAF_DIR = Path("/share/costeffectiveness/auxiliary_data/GBD_2019/01_original_data/population_attributable_fraction/risk_factor/lbwsg/data/")
 
